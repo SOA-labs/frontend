0aaadd10449bebeae81e9c858c8a8b04e701aa10
stepagin <dickiysad@gmail.com>
2024-10-04 23:09:16 +0300

    init commit second service

---
+++ second-service/src/test/java/ru/artemiyandstepan/secondservice/SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artemiyandstepan.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
